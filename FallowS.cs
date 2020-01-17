13a8da3c7cd8d67ddf72693443642e7768e03c7d
Maksymilian Mazur <37407325+maksmazur@users.noreply.github.com>
2020-01-17 12:13:20 +0100

    Add new files

---
+++ FallowS.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowS : MonoBehaviour
+{
+    [SerializeField]
+    private float xMax;
+    [SerializeField]
+    private float yMax;
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float yMin;
+
+
+
+    private void Start()
+    {
+
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax),transform.position.z);
+    }
+
+    //co sledzimy
+    public Transform target;
+/*
+      Vector3 velocity = Vector3.zero;
+
+      public float smoothTime = .15f;
+
+      //czas sledzenia celu
+
+      private void Start()
+      {
+
+          target = GameObject.Find("Player").transform;
+      }
+
+      void fixedUpdate()
+      {
+          //pozycja celu
+          Vector3 targetPos = target.position;
+
+          targetPos.z = transform.position.z;
+
+          transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
+     */
+}
+
---
+++ Movesphere.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Movesphere : MonoBehaviour
+
+{
+    public float playerSpeed;
+    public float Jump;
+
+
+    private bool isJumping;
+    private float move;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        move = CrossPlatformInputManager.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * playerSpeed, rb.velocity.y);
+
+        if (move < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+
+        }
+
+        else if (move > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+          if (CrossPlatformInputManager.GetButtonDown("Jump") && !isJumping)
+          {
+              rb.AddForce(new Vector2(rb.velocity.x, Jump));
+              isJumping = true;
+          }
+        
+
+       /* if (CrossPlatformInputManager.GetButtonDown("Jump") && rb.velocity.y == 0)
+            rb.AddForce(Vector2.up * 7000f);
+        */
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+       
+    }
+}
